66fafd092cc62c598db4cefa292830c4fe7e8c10
Frank <frank@elsinga.de>
2019-12-24 12:57:16 +0100

    added a new file-selection option

---
+++ main.py
@@ -0,0 +1,35 @@
+import glob
+import os
+
+inputPath = os.path.dirname(__file__) + "/Footage"
+
+
+def number_of_txt_in_input() -> int:
+    return len(glob.glob1(inputPath, "*.mp4"))
+
+
+# noinspection PyBroadException
+try:
+    txtFiles: int = number_of_txt_in_input()
+except:
+    print("something went wrong when trying to access the 'TUMSpeak_InputFiles' - Folder")
+    txtFiles: int = 0
+
+if txtFiles > 0:
+    print("\nInput-Source is the '%s' - Folder" % inputPath)
+    if txtFiles > 1:
+        print("('TUMSpeak_InputFiles' - Folder has %d .mp4 Files)" % txtFiles)
+    else:
+        print("('TUMSpeak_InputFiles' - Folder has 1 .mp4 File)")
+    filecounter = 1
+    for i in glob.glob1(inputPath, "*.mp4"):
+        print("\n-----------------------------------------"
+              "\nFile #", filecounter)
+        filecounter += 1
+        pro.process(open(inputPath + "/" + i).read())
+
+else:
+    print("Input-Source is the commandline")
+    print("('TUMSpeak_InputFiles' - Folder has %d .txt Files)" % txtFiles)
+    textToBeConverted = input("Please input the Text, you would like to have converted to TUMSpeak\n\t>")
+    pro.process(textToBeConverted)
