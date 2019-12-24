3ec6b1e57ea7989baa505bc370a564982050012b
Frank <frank@elsinga.de>
2019-12-24 14:41:37 +0100

    fixed a bug in the calling logic, which prevented glob to look in the right place for the .mp4 files

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import glob
+import os
 
 import process as pro
 
-inputPath = "/Footage/"
-
-
-def number_of_txt_in_input() -> int:
-    return len(glob.glob1(inputPath, ".mp4"))
-
+inputPath = os.path.dirname(__file__) + "/Footage"
 
 frameRate = 30
 SAMPLE_RATE = 44100
@@ -20,29 +16,30 @@ OUTPUT_FILE = ""
 
 # noinspection PyBroadException
 try:
-    txtFiles: int = number_of_txt_in_input()
+    number_of_files: int = len(glob.glob1(inputPath, "*.mp4"))
+    print(number_of_files)
 except:
     print("something went wrong when trying to access the '%s' - Folder" % inputPath)
-    txtFiles: int = 0
+    number_of_files: int = 0
 
-if txtFiles > 0:
+if number_of_files > 0:
     print("\nInput-Source is the '%s' - Folder" % inputPath)
-    if txtFiles > 1:
-        print("('%s' - Folder has %d .mp4 Files)" % (inputPath, txtFiles))
+    if number_of_files > 1:
+        print("('%s' - Folder has %d .mp4 Files)" % (inputPath, number_of_files))
     else:
         print("('%s' - Folder has 1 .mp4 File)" % inputPath)
     filecount = 1
-    for i in glob.glob1(inputPath, ".mp4"):
+    for i in glob.glob1(inputPath, "*.mp4"):
         print("\n-----------------------------------------"
               "\nFile #", filecount)
         filecount += 1
-        INPUT_FILE = inputPath + i
+        INPUT_FILE = inputPath + "/" + i
         pro.process(OUTPUT_FILE, SILENT_THRESHOLD, NEW_SPEED, FRAME_SPREADAGE,
                     SAMPLE_RATE, frameRate, FRAME_QUALITY, INPUT_FILE)
 
 else:
     print("Input-Source is the commandline")
-    url = input("Please input the URL (for example 'example.mp4')\n\t>")
+    url = input("Please input the URL (for example 'https://www.youtube.com/watch?v=DQ8orIurGxw')\n\t>")
     INPUT_FILE = pro.download_file(url)
     pro.process(OUTPUT_FILE, SILENT_THRESHOLD, NEW_SPEED, FRAME_SPREADAGE,
                 SAMPLE_RATE, frameRate, FRAME_QUALITY, INPUT_FILE)
