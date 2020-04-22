57e24570c955e109d7f16cdbe366ade64a10e68a
frank <frank@elsinga.de>
2020-04-22 10:45:49 +0200

    addded basic unit-tests setup
    Signed-off-by: frank <frank@elsinga.de>

---
+++ tests/test_jumpcutter.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_get_max_volume(self):
+        self.fail()
+
+    def test_copy_frame(self):
+        self.fail()
+
+    def test_input_to_output_filename(self):
+        self.fail()
+
+    def test_create_path(self):
+        self.fail()
+
+    def test_delete_path(self):
+        self.fail()
+
+    def test_download_file(self):
+        self.fail()
+
+    def test_count_mp4_files_in_folder(self):
+        self.fail()
+
+    def test_call_subprocess(self):
+        self.fail()
+
+    def test_process(self):
+        self.fail()
+
+    def test_process_folder(self):
+        self.fail()
+
+    def test_process_yt(self):
+        self.fail()
+
+    def test_process_settings(self):
+        self.fail()
